40566a0454a0155315baadd4ad5936c16907f8aa
Sandro Alencar Fernandes <sandro@arealocal.com.br>
2024-05-09 21:53:56 -0300

    Implementação inicial da consulta aos contatos

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\ContatosController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::resource('/contatos',ContatosController::class,);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
